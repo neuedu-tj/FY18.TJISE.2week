e67911c7929eb4002375d82ed6c4ec3a6cdcf4e5
cofcoltd <cofcoltd@qq.com>
2018-01-09 11:37:31 +0800

    jquery -01

--- FY18.TJISE.2week/case/wechat/js/chat.js
+++ FY18.TJISE.2week/case/wechat/js/chat.js
@@ -3,4 +3,12 @@ $(function(){
 		console.log("图片的路径 : "  + $(this).data("photo"))
 		$(this).css('background-image' , 'url('+$(this).data("photo")+')')
 	})
+	
+	$(".content").click(function(){
+////		$('.content').animate({scrollTop: $(document).height()}, -300); 
+//		$("#content").scrollTop(-100);
+//		console.log("----------")
+//		
+	})
+	
 })
---
+++ FY18.TJISE.2week/page/04.jquery/02/index.js
@@ -0,0 +1,26 @@
+$(function(){
+	
+	console.log( $('#d1').height()  )
+		console.log( $('#d1').innerHeight()  )
+				console.log( $('#d1').outerHeight()  )
+						console.log( $('#d1').outerHeight(true)  )
+						
+						
+						
+	$("#btn").click(function(){
+//		$('#d1').toggle()   //   toggle = show + hide 
+		
+//		$('#d1').toggle(400)   // 过度
+
+//		$('#d1').toggle(1000 , function(){
+//			alert("动画执行完毕 ? 后面咱干啥？ ")
+//		})
+
+//		$('#d1').slideToggle()
+//		
+//		$('#d1').fadeToggle(1000)
+		
+	})
+	
+	
+})
